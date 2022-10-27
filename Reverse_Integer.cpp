c669ff9917834472b4f9a42ab83134cb4d13393c
Mahesh padimi <82667979+maheshpadimi@users.noreply.github.com>
2022-10-27 14:14:31 +0530

    #include<iostream> using namespace std; int main() {     int n,r,rev=0;     cin>>n;     while (n!=0)     {         r = n%10;         rev = rev*10+r;         n=n/10;     }     if (n<0)     {         cout<<-rev;     }     else     {         cout<<rev;     } }

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,r,rev=0;
+    cin>>n;
+    while (n!=0)
+    {
+        r = n%10;
+        rev = rev*10+r;
+        n=n/10;
+    }
+    if (n<0)
+    {
+        cout<<-rev;
+    }
+    else
+    {
+        cout<<rev;
+    }
+}
